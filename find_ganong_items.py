c7f12c27231a5abb200353d09a44554dd272de33
Constantine Lignos <lignos@seas.upenn.edu>
2014-01-27 20:36:46 -0500

    Improve Ganong effect items finder.

--- find_ganong_items.py
+++ find_ganong_items.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-"""Search over a lexicon for Ganong effect items."""
+"""
+Search over a lexicon for Ganong effect items.
+
+"""
 
 # Copyright 2014 Constantine Lignos
 #
@@ -16,8 +19,11 @@
 # limitations under the License.
 
 import argparse
+import csv
 
 from lingtools.corpus.cmudictreader import CMUDict
+from lingtools.corpus.elp import ELP
+from lingtools.corpus.subtlexreader import SubtlexDict
 from lingtools.phon import syllabify
 from lingtools.phon.arpabet import remove_stress
 
@@ -31,19 +37,42 @@ BAD_CHARACTERS = set((
     "'",
     '-',
     '"',
-    '.',
-    '(',
-    ')',
-    '}',
-    '{',
     ))
 
 VOWELS = syllabify.vowels
 
 
-def find_ganong(cmudict_path, max_sylls):
+def find_ganong(elp_path, cmudict_path, subtlex_path, max_sylls, out_path):
     """Find Ganong effect items a dictionary with <= max_sylls syllables."""
-    word_prons = CMUDict(cmudict_path)
+    # Open corpora
+    elp = ELP(elp_path)
+    cmudict = CMUDict(cmudict_path)
+    subtlex = SubtlexDict(subtlex_path)
+
+    # Collect all monomorphemic words with few enough syllables and no
+    # excluded characters
+    words = set(word.text for word in elp.itervalues()
+                if (word.nsyll <= max_sylls and word.monomorph and
+                    not any(char in BAD_CHARACTERS for char in word.text)))
+
+    # Make sure items can be syllabified and record an authoritative syllable count
+    word_sylls = {}
+    word_prons = {}
+    # Iterate over a copy to allow for modification
+    for word in words.copy():
+        try:
+            pron = cmudict[word]
+            sylls = syllabify.syllabify(pron)
+        except (KeyError, ValueError):
+            # Not in CMUDict or could not be syllabified
+            words.remove(word)
+            continue
+        else:
+            if len(sylls) > max_sylls:
+                words.remove(word)
+            else:
+                word_prons[word] = pron
+                word_sylls[word] = sylls
 
     # Add the reverse mapping for voicing pairs
     for key, val in VOICING_PAIRS.items():
@@ -51,32 +80,36 @@ def find_ganong(cmudict_path, max_sylls):
 
     # Filter to words that start with the right segment and have a
     # simplex onset
-    eligible_word_prons = {word: pron for word, pron in word_prons.iteritems()
-                           if (pron[0] in VOICING_PAIRS and
-                               remove_stress(pron[1]) in VOWELS)}
-
-    # Count number of syllables in every word, deleting any that
-    # cannot be syllabified.
-    word_nsylls = {}
-    for word, pron in word_prons.items():
-        try:
-            nsylls = len(syllabify.syllabify(pron))
-        except ValueError:
-            del word_prons[word]
-            continue
-        else:
-            word_nsylls[word] = nsylls
+    eligible_words = {word for word, pron in word_prons.iteritems()
+                      if (pron[0] in VOICING_PAIRS and
+                          remove_stress(pron[1]) in VOWELS)}
 
     # Set all of prons for checking if a pron is in the lexicon
-    all_prons = set(tuple(value) for value in word_prons.itervalues())
+    all_prons = set(tuple(value) for value in cmudict.itervalues())
 
-    # Find the pairs. If max_sylls is None, skip the syllable length check.
-    ganong_words = [word for word, pron in word_prons.iteritems()
-                    if (word_nsylls[word] <= max_sylls and True and
-                        not _exclude_word(word) and
-                        is_ganong_pron(pron, all_prons, VOICING_PAIRS))]
-                    
-    return ganong_words
+    # Find the pairs
+    ganong_words = set(word for word in eligible_words
+                       if is_ganong_pron(word_prons[word], all_prons, VOICING_PAIRS))
+
+    # Get frequency information
+    word_freqs = {}
+    for word in ganong_words:
+        try:
+            word_freqs[word] = subtlex[word].freq_count_low
+        except KeyError:
+            pass
+
+    # Write the output
+    with open(out_path, 'wb') as out_file:
+        fields = ['word', 'onset', 'nucleus', 'coda', 'sbtlx.freq_low']
+        writer = csv.writer(out_file)
+        writer.writerow(fields)
+        for word, freq in word_freqs.iteritems():
+            # Decompose the first syllable
+            onset, nucleus, coda = [''.join(item) for item in word_sylls[word][0]]
+            # Strip stress off nucleus
+            nucleus = remove_stress(nucleus)
+            writer.writerow([word, onset, nucleus, coda, freq])
 
 
 def _exclude_word(word):
@@ -99,13 +132,15 @@ def is_ganong_pron(pron, all_prons, voicing_pairs):
 def main():
     """Parse arguments and call the extractor."""
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('cmudict', help='input CSV file')
+    parser.add_argument('elp_path', help='ELP item CSV file')
+    parser.add_argument('cmudict_path', help='CMUDict file')
+    parser.add_argument('subtlex_path', help='SUBTLEX file')
+    parser.add_argument('out_path', help='output CSV file')
     parser.add_argument('-s', '--syll', nargs='?', default=None, type=int,
                         help='maximum number of syllables')
     args = parser.parse_args()
-    items = find_ganong(args.cmudict, args.syll)
-    for word in sorted(items):
-        print word
+    find_ganong(args.elp_path, args.cmudict_path, args.subtlex_path,
+                args.syll, args.out_path)
 
 
 if __name__ == "__main__":
