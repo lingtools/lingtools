edfc7a0cd8a6420be4e4c0cdc75bd7b014e7035c
Constantine Lignos <constantine.lignos@gmail.com>
2014-07-14 11:33:57 -0400

    Exclude external modules from static analysis.

--- test_lingtools.sh
+++ test_lingtools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Running pylint..."
-pylint *.py lingtools
+pylint *.py lingtools --ignore=syllabify.py,textgrid.py
 echo "Running flake8..."
-flake8 *.py lingtools
+flake8 *.py lingtools --exclude=syllabify.py,textgrid.py
 nosetests --with-doctest
